2a02aaa8ca13f7e2bec5f200537e40e016761849
awannabee <onehundredandfourtytwo@gmail.com>
2015-10-01 15:16:06 +0300

    Movement Fix

--- SnakeProject/SnakeProject.cs
+++ SnakeProject/SnakeProject.cs
@@ -2,81 +2,148 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
-class SnakeProject
+namespace SnakeProject
 {
-    static void Main(string[] args)
+    class Point
     {
-        Console.BufferHeight = Console.WindowHeight = 25;
-        Console.BufferWidth = Console.WindowWidth = 100;
-        int height = Console.WindowHeight/2;
-        int width = Console.WindowWidth;
-
-        char[,] matrix = new char[height, width];
-        for (int row = 0; row < height; row++)
+        public int snakeX;
+        public int snakeY;
+        public Point(int x, int y)
         {
-            for (int col = 0; col < width; col++)
-            {
-                if (col==0 || col == width - 1)
-                {
-                    matrix[row, col] = '#'; // sides
-                }
-                if (row == 0 || row == height - 1)
-                {
-                    matrix[row, col] = '#'; //top & bottom
-                }
-                
-                Console.Write(matrix[row,col]);
-            }
-            Console.WriteLine();
+            snakeX = x;
+            snakeY = y;
         }
-        int snakeX = 0;
-        int snakeY = 0;
+    }
 
-        while (true)
+    class SnakeProject
+    {
+        static void Main(string[] args)
         {
-            ConsoleKeyInfo pressedKey = Console.ReadKey();
-            if (pressedKey.Key == ConsoleKey.UpArrow || pressedKey.Key == ConsoleKey.DownArrow)
-            {
-                if (pressedKey.Key == ConsoleKey.UpArrow)
-                {
-                    snakeY--;
-                    if (snakeY < 0)
-                    {
-                        snakeY = 0;
-                    }
-                }
-                else if (pressedKey.Key == ConsoleKey.DownArrow)
-                {
-                    snakeY++;
-                }
-                Console.Clear();
 
-                Console.SetCursorPosition(snakeX, snakeY);
-                Console.Write(SnakeType(type));
+            Point[] directions = new Point[]
+            {
+                new Point(1, 0), // goingDown
+                new Point(-1, 0), // goingUp
+                new Point(0, 1), // goingRight
+                new Point(0, -1) // goingLeft
+            };
+            int direction = 2; // holds the direction which the snake is moving
+
+            Queue<Point> SnakeBody = new Queue<Point>();
+            for (int i = 0; i < 5; i++)
+            {
+                SnakeBody.Enqueue(new Point(0, i));
+            }
+            foreach (Point position in SnakeBody)
+            {
+                Console.SetCursorPosition(position.snakeY, position.snakeX);
+                Console.Write("*");
             }
 
-            else if (pressedKey.Key == ConsoleKey.LeftArrow || pressedKey.Key == ConsoleKey.RightArrow)
+            while (true)
             {
-                if (pressedKey.Key == ConsoleKey.LeftArrow)
+                if (Console.KeyAvailable)
                 {
-                    snakeX--;
-                    if (snakeX < 0)
+                    ConsoleKeyInfo Input = Console.ReadKey();
+                    if (Input.Key == ConsoleKey.S)
                     {
-                        snakeX = 0;
+                        direction = 0;
+                    }
+                    if (Input.Key == ConsoleKey.A)
+                    {
+                        direction = 3;
+                    }
+                    if (Input.Key == ConsoleKey.W)
+                    {
+                        direction = 1;
+                    }
+                    if (Input.Key == ConsoleKey.D)
+                    {
+                        direction = 2;
                     }
                 }
-                else if (pressedKey.Key == ConsoleKey.RightArrow)
-                {
-                    snakeX++;
-                }
+                SnakeBody.Dequeue();
+                Point head = SnakeBody.Last();
+                Point newDirection = directions[direction];
+                Point newHeadPosition = new Point(head.snakeX + newDirection.snakeX, head.snakeY + newDirection.snakeY);
+                SnakeBody.Enqueue(newHeadPosition);
+
                 Console.Clear();
 
-                Console.SetCursorPosition(snakeX, snakeY);
-                Console.Write(SnakeType(type));
-            }
+                foreach (Point position in SnakeBody)
+                {
+                    Console.SetCursorPosition(position.snakeY, position.snakeX);
+                    Console.Write("*");
+                }
 
+                Thread.Sleep(50);
+            }
         }
     }
 }
+
+
+            //for (int row = 0; row < height; row++)
+            //{
+            //    for (int col = 0; col < width; col++)
+            //    {
+            //        if (col == 0 || col == width - 1)
+            //        {
+            //            matrix[row, col] = '#'; // sides
+            //        }
+            //        if (row == 1 || row == height - 1)
+            //        {
+            //            matrix[row, col] = '#'; //top & bottom
+            //        }
+            //        
+            //    }
+            //}
+            //
+            //////////////////////////////////////////////////////////////////////////////////////////////////////////
+            //while (true)
+            //{
+            //    ConsoleKeyInfo pressedKey = Console.ReadKey();
+            //    if (pressedKey.Key == ConsoleKey.UpArrow || pressedKey.Key == ConsoleKey.DownArrow)
+            //    {
+            //        if (pressedKey.Key == ConsoleKey.UpArrow)
+            //        {
+            //            snakeY--;
+            //            if (snakeY < 0)
+            //            {
+            //                snakeY = 0;
+            //            }
+            //        }
+            //        else if (pressedKey.Key == ConsoleKey.DownArrow)
+            //        {
+            //            snakeY++;
+            //        }
+            //        Console.Clear();
+
+            //        Console.SetCursorPosition(snakeX, snakeY);
+            //        Console.Write("$");
+            //    }
+
+            //    else if (pressedKey.Key == ConsoleKey.LeftArrow || pressedKey.Key == ConsoleKey.RightArrow)
+            //    {
+            //        if (pressedKey.Key == ConsoleKey.LeftArrow)
+            //        {
+            //            snakeX--;
+            //            if (snakeX < 0)
+            //            {
+            //                snakeX = 0;
+            //            }
+            //        }
+            //        else if (pressedKey.Key == ConsoleKey.RightArrow)
+            //        {
+            //            snakeX++;
+            //        }
+            //        Console.Clear();
+
+            //        Console.SetCursorPosition(snakeX, snakeY);
+            //        Console.Write("$");
+            //    }
+
+            //}
